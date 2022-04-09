47d580a46dbc39d57594921c036a9bde1ef0e87d
Jazzberries <anime.flick.chick@gmail.com>
2022-04-10 09:38:38 +1000

    Updated project links to more recent projects and added updated project images

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,3 +43,31 @@ aboutMeLink.addEventListener("click", displayAbout);
 projectsLink.addEventListener("click", displayProjects);
 
 contactLink.addEventListener("click", displayContact);
+
+
+// change the static project images to gifs when clicked on
+
+const codeQuizImage = document.getElementById("code-quiz-image");
+
+const weatherDashboardImage = document.getElementById("weather-dashboard-image");
+
+const mainProjectImage = document.getElementById("main-project");
+
+function changeImageToGif(event) {
+
+    event.stopPropagation();
+
+    if (codeQuizImage.classList.contains("static")) {
+        codeQuizImage.setAttribute("src", "./assets/images/code-quiz-demo.gif");
+        codeQuizImage.classList.add("gif");
+        codeQuizImage.classList.remove("static");
+    } else {
+        codeQuizImage.setAttribute("src", "./assets/images/code-quiz-splash-page.png");
+        codeQuizImage.classList.add("static");
+        codeQuizImage.classList.remove("gif");
+    }
+}
+
+codeQuizImage.addEventListener("click", changeImageToGif);
+
+weatherDashboardImage.addEventListener("click", changeImageToGif);
