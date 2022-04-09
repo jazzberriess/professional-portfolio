5f90f605dc16bd074e3071add0d72d217bc4c065
Jazzberries <anime.flick.chick@gmail.com>
2022-04-09 18:30:59 +1000

    added in functions to hide and display about me, projects and contact details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,43 @@
 //hide the different sections until their heading is clicked on, then show them
+
+const aboutMe = document.getElementById("about-me");
+
+const aboutMeLink = document.getElementById("about-me-link");
+
+
+const projects = document.getElementById("projects");
+
+const projectsLink = document.getElementById("my-projects-link");
+
+const contactDetails = document.getElementById("contact-details");
+
+const contactLink = document.getElementById("contact-me-link");
+
+function displayAbout(event) {
+    event.stopPropagation();
+
+    aboutMe.classList.remove("hidden");
+    projects.classList.add("hidden");
+    contactDetails.classList.add("hidden");
+}
+
+function displayProjects(event) {
+    event.stopPropagation();
+
+    projects.classList.remove("hidden");
+    aboutMe.classList.add("hidden");
+}
+
+function displayContact(event) {
+
+    event.stopPropagation();
+
+    contactDetails.classList.remove("hidden");
+    projects.classList.add("hidden");
+}
+
+aboutMeLink.addEventListener("click", displayAbout);
+
+projectsLink.addEventListener("click", displayProjects);
+
+contactLink.addEventListener("click", displayContact);
